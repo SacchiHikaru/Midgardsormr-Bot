a3499380f0ee0974600fc7a9abe7a8937783c351
SacchiHikaru <sacchilight@gmail.com>
2016-09-22 06:23:26 -0300

    Change to !music list command
    Found out a (dumb) way to make Middy Bot list all songs in one message,
    instead of in multiple. It's functional but the code for it is kind of
    ugly. idk if it can really be fixed. Java is dumb.

--- src/main/java/sacchi/bot/services/Music.java
+++ src/main/java/sacchi/bot/services/Music.java
@@ -10,45 +10,45 @@ import java.nio.file.Paths;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-public class Music{
+public class Music {
 	ArrayList<Song> songList = new ArrayList<Song>(0);
 	Gson gson = new GsonBuilder().setPrettyPrinting().create();
 	String fileData = null;
 	Song[] songs = null;
+
 	public Music() {
-		try{
+		try {
 			fileData = new String(Files.readAllBytes(Paths.get("songList.json")), StandardCharsets.UTF_8);
 			songs = gson.fromJson(fileData, Song[].class);
-			for(int i = 0; i < songs.length; i++){
+			for (int i = 0; i < songs.length; i++) {
 				songList.add(songs[i]);
 			}
-		} catch(IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
 			throw new RuntimeException("Failed to do stuff.");
 		} finally {
-			
+
 		}
 	}
-	
-	public void musicHandler(Message message){
-		if(message.getContent().equalsIgnoreCase("!music")
-		|| message.getContent().equalsIgnoreCase("!music ")){
-			message.reply("Usage: !Music [songname]"
-					+ "\nCan also use !Music List for all song names and descriptions"
+
+	public void musicHandler(Message message) {
+		if (message.getContent().equalsIgnoreCase("!music") || message.getContent().equalsIgnoreCase("!music ")) {
+			message.reply("Usage: !Music [songname]" + "\nCan also use !Music List for all song names and descriptions"
 					+ "\nAnd !Music random for a random song."
 					+ "\nIf you have the necessary permissions, you can use !Music add to add new songs."
 					+ "\nInstructions on how to add songs are given when using !music add (all lowercase).");
-		} else if(message.getContent().equalsIgnoreCase("!music random")){
+		} else if (message.getContent().equalsIgnoreCase("!music random")) {
 			message.reply(musicRandom(message));
 		} else if (message.getContent().equalsIgnoreCase("!music list")) {
 			musicList(message);
-		} else if(message.getContent().startsWith("!Music add")){
+		} else if (message.getContent().startsWith("!Music add")) {
 			message.reply(addSong(message));
-		} else if(message.getContent().toLowerCase().contains("!music add")){
+		} else if (message.getContent().toLowerCase().contains("!music add")) {
 			message.reply("Due to code limitations, this command is case-sensitive."
 					+ "\nPlease use ``!Music add [song name]-----[song description]-----[song link]------[pastebin link to song lyrics]`` to properly add a song to the list."
 					+ "\nBefore you ask - that is **FIVE** dashes (-). There are no spaces between the dashes and the info provided."
@@ -58,49 +58,53 @@ public class Music{
 			message.reply(musicSpecific(message));
 		}
 	}
-	
-	public String musicRandom(Message message){
-		int randomNumber = (int)(Math.random()*songList.size());
+
+	public String musicRandom(Message message) {
+		int randomNumber = (int) (Math.random() * songList.size());
 		return songList.get(randomNumber).toString();
 	}
-	
-	public String musicSpecific(Message message){
-		for(int i = 0; i < songList.size(); i++){
-			if(songList.get(i).getName().toLowerCase().contains(message.getContent().toLowerCase().replace("!music ", "")) 
-			|| songList.get(i).getDescription().toLowerCase().contains(message.getContent().toLowerCase().replace("!music ", ""))){
+
+	public String musicSpecific(Message message) {
+		for (int i = 0; i < songList.size(); i++) {
+			if (songList.get(i).getName().toLowerCase()
+					.contains(message.getContent().toLowerCase().replace("!music ", ""))
+					|| songList.get(i).getDescription().toLowerCase()
+							.contains(message.getContent().toLowerCase().replace("!music ", ""))) {
 				return songList.get(i).toString();
 			}
 		}
 		return "Could not find song.";
 	}
-	
-	public void musicList(Message message){
-		for(int i = 0; i < songList.size(); i++){
-			message.reply("\n" + songList.get(i).getName() + " --- " + songList.get(i).getDescription());
-		}
+
+	public void musicList(Message message) {
+
+		String s = String.join(", ", songList.stream().map(x -> "\n" + x.getName() + " ----- " + x.getDescription())
+				.collect(Collectors.toList()));
+		message.reply(s);
 	}
-	
-	public String addSong(Message message){
+
+	public String addSong(Message message) {
 		FileWriter writer = null;
 		File file = new File("songList.json");
-		String[] split = new String [4];
-		if(message.getAuthor().getRoles(message.getChannelReceiver().getServer()).contains(message.getChannelReceiver().getServer().getRoleById("214097524955283456"))){
-			try{
-				writer = new FileWriter(file,false);
+		String[] split = new String[4];
+		if (message.getAuthor().getRoles(message.getChannelReceiver().getServer())
+				.contains(message.getChannelReceiver().getServer().getRoleById("214097524955283456"))) {
+			try {
+				writer = new FileWriter(file, false);
 				split = message.getContent().replace("!Music add ", "").split("-----");
 				Song addedSong = new Song(split[0], split[1], split[2], split[3]);
 				songList.add(addedSong);
 				writer.write(gson.toJson(songList));
 				return "Song added.";
-			} catch (IOException e){
+			} catch (IOException e) {
 				throw new RuntimeException("Failed to add song.");
 			} finally {
 				try {
-					if(writer != null)
+					if (writer != null)
 						writer.flush();
-						writer.close();
-				} catch (IOException e){
-					
+					writer.close();
+				} catch (IOException e) {
+
 				}
 			}
 		} else {
